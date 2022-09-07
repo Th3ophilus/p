eb630cd9879ff90cfe7018fe0dcf6e879de783e3
Th3ophilus <banmeketheo@gmail.com>
2022-09-07 03:24:29 -0700

    Testing git

---
+++ index.js
@@ -0,0 +1,103 @@
+// // var paragraph = document.getElementsByClassName("para");
+
+
+
+
+
+// alert("HEllo")
+
+// console.log("Hello");
+
+
+
+
+// alert("Welcome To The Bar")
+// var age = prompt("Enter Your Age")
+// if( age >= 21){
+//    alert("Welcome, you can drink as much as you like")  
+// }
+// else{
+//   alert("go and read your books")
+// }
+
+// var enteryourscore = prompt("Enter Your Score")
+// var Score = ("enteryourscore")
+// if(enteryourscore>=80){
+//     alert("you got an A")
+// } else if(enteryourscore>=75&& enteryourscore<=79){
+//     alert("you got a B")
+// // }else if(enteryourscore>=50&& enteryourscore<=74){
+//     alert("you got a C")
+// }
+// else
+// { alert("you failed boss")}
+
+
+
+
+// var days = ["monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+// console.log(days.length)
+
+// for(i=0; i< days.length; i++ ){
+//     console.log(days[i])
+// }
+
+
+
+
+
+
+
+// var day = prompt("Enter Day Of Week")
+
+
+// if(day== "monday"){
+//         alert("Go to work")
+//     } else if(day =="tuesday"){
+//         alert("Go to work")
+//     }else if(day =="wednesday"){
+//         alert("Go to bank")
+//     }
+//     else if(day =="thursday"){
+//         alert("Go to sleep")
+//     }
+//     else if(day =="friday"){
+//         alert("Watch a movie")
+//     }
+//     else if(day =="saturday"){
+//         alert("Go to gym")
+//     }
+//     else{ 
+//         alert("Go to church")
+//     }
+
+
+ 
+    
+
+// var username = prompt("Enter your first  name")
+
+// firstLetterUpper(username)
+
+
+
+// function firstLetterUpper(username) {
+//     var firstLetter=username.slice(0,1)
+//     var firstLetterUppercase=firstLetter.toUpperCase()
+    
+//     var rest = username.slice(1)
+//     var restLowercase= rest.toLowerCase()
+//     var hello= "hello"
+//     alert("hello " + firstLetterUppercase + restLowercase)
+
+    
+// }
+
+
+// var enterYourName2 = prompt("Enter your surname")
+// firstLetterUpper(enterYourName2)
+
+
+
+var hello = document.getElementById("hello")
+hello.style.color = "red"
